d975d2060434dfa57041bb7dbe08dc19dd47ccf6
Ubais <ubais@aventusinformatics.com>
2026-06-12 11:21:19 +0530

    url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
   ? ""
-  : "https://ubaise-portfolio.onrender.com"; // Replace with your actual Render web service URL once deployed
+  : "https://mohammedubaise.github.io/"; // Replace with your actual Render web service URL once deployed
